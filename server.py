866df5ddcc1bc448a90e1487b053b088d1eb48ed
rdrxss <rodrichukin@gmail.com>
2024-07-08 00:26:22 +0000

    Mundo guardado 2024-07-08

--- server.py
+++ server.py
@@ -36,7 +36,7 @@ if not os.path.exists("./.gitignore"):
 def download_latest_release(download_path='.'):
 	mirror = "https://elyxdev.github.io/latest"
 	pet = requests.get(mirror)
-	if pet.status_code == 200:
+	if pet.status_code == 200:	
 		data = pet.json()
 		url = data.get('latest')
 		version = url.split("/")[-1]
